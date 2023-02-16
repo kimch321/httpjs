de48ce9390ec4049b8fd2a6e2ce44a260b4aefff
KIM CHUNG IL <cnddlf12@naver.com>
2023-02-16 17:42:52 +0900

    성적처리1 - body-parser

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,24 @@ router.get('/',(req,res) => {
    // handlebars 뷰 엔진으로 응답처리
    res.render('index',{title:'index'});
 });
+
+router.get('/sungjuk',(req,res) => {
+   res.render('sungjuk',{title:'성적처리'});
+});
+
+router.post('/sungjuk',(req,res,next) => {
+   // 폼으로 전송된 데이터들은 req.body, req.body.폼이름 등으로 확인 가능
+   // console.log(req.body,typeof(req.body));
+   // console.log(req.body.name,req.body.kor,req.body.eng,req.body.mat);
+   let {name, kor, eng, mat} = req.body;
+   // console.log(name, kor, eng, mat);
+
+   let [tot, avg, grd] = [Number(kor)+Number(eng)+Number(mat), (Number(kor)+Number(eng)+Number(mat))/3, '가']
+   console.log(tot, avg, grd)
+
+   res.redirect(304,'/');
+});
+
 // router.get('/',(req,res) => {
 //    // 응답으로 지정한 파일의 내용을 전송함
 //    res.sendFile(path.join(__dirname,'../public','smile.png'));
